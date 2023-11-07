2c2d720b4f3d0a1971baf3716016616a7d6c1b38
Fredy <afredybustos@gmail.com>
2023-11-06 21:35:38 -0500

    add: next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        // implement database connection
+        return { user: credentials?.email } as any;
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: ({ token, user }) => {
+      const currentUser = user as any
+      if (user) {
+        token.user = {
+          id: currentUser.id,
+          email: currentUser.email,
+          name: currentUser.name,
+          lastname: currentUser.lastname,
+        }
+      }
+      return token
+    },
+    session: ({ session, token }) => {
+      session.user = token.user as any
+      return session
+    }
+  },
+  pages: {
+    signIn: '/signin',
+    signOut: '/signup'
+  }
+})
+
+
+export { handler as GET, handler as POST }
---
+++ app/api/auth/signin/index.tsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const user = await request.json()
+  try {
+    return NextResponse.json({ data: user, status: 'success' }, { status: 200 })
+  } catch (error) {
+    throw new Error(JSON.stringify(error))
+  }
+}
