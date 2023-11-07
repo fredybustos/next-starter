7233192489d7973ca47c37666501ac3811821790
Fredy <afredybustos@gmail.com>
2023-11-06 21:39:07 -0500

    add: home components

---
+++ app/home/components/navlink/index.tsx
@@ -0,0 +1,51 @@
+import {
+  UserGroupIcon,
+  HomeIcon,
+  PowerIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import './styles.css'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  { name: 'Home', href: '/home', icon: HomeIcon },
+  {
+    name: 'Clients',
+    href: '/home/clients',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Users', href: '/home/users', icon: UserGroupIcon },
+];
+
+export default function NavLinks({ isOpen, onToggle }: { isOpen: boolean, onToggle: () => void }) {
+  const pathname = usePathname()
+
+  const open = isOpen ? 'w-[80%]' : 'w-0 overflow-hidden'
+
+  return (
+    <div className={`navLink-ctn ${open}`}>
+      <div className='flex flex-col gap-2'>
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          const selected = pathname === link.href ? 'navLink-selected' : ''
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              onClick={onToggle}
+              className={`navLink-item ${selected}`}
+            >
+              <LinkIcon className="w-6 mr-4" />
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+      <Link className="navLink-item" href='/signout'>
+        <PowerIcon className="w-6 mr-4" />
+        Sign Out
+      </Link>
+    </div>
+  );
+}
---
+++ app/home/components/sideNav/index.tsx
@@ -0,0 +1,31 @@
+"use client"
+import Link from 'next/link';
+import NavLinks from '@/app/home/components/navlink';
+import { useState } from 'react';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import './styles.css'
+
+export default function SideNav() {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleToggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div className="sidenav-ctn">
+      <div className='sidenav-logo'>
+        <div className='flex flex-row items-center'>
+          <Bars3Icon onClick={handleToggle} className="sidenav-icon" />
+          <Link
+            href="/"
+            className='ml-2'
+          >
+            <div className="w-32 text-white md:w-40">
+              logo
+            </div>
+          </Link>
+        </div>
+        <span className='hidden'>Notify</span>
+      </div>
+      <NavLinks isOpen={isOpen} onToggle={handleToggle} />
+    </div>
+  );
+}
